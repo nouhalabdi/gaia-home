7d92eed77687a611509966c96bb7dcbb5e86e860
Nouha <nouhalabdi5@gmail.com>
2026-09-21 01:20:36 +0100

    respo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -125,7 +125,7 @@ export default async function Home() {
             Aucun produit disponible pour le moment.
           </div>
         ) : (
-          <div className="grid grid-cols-2 lg:grid-cols-4 gap-3 sm:gap-4 md:gap-6">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 md:gap-6">
             {products.map((product: any) => (
               <Link 
                 href={`/product/${product._id}`} 
