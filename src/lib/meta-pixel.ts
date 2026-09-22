7b0a03de68c5591631680c82edbc1ca593bf9948
Nouha <nouhalabdi5@gmail.com>
2026-09-22 17:17:19 +0100

    add

--- src/lib/meta-pixel.ts
+++ src/lib/meta-pixel.ts
@@ -1,25 +1,27 @@
 
-import ReactPixel from 'react-facebook-pixel';
 
-export const initMetaPixel = () => {
+export const initMetaPixel = async () => {
+  if (typeof window === 'undefined') return;
+
   const pixelId = process.env.NEXT_PUBLIC_META_PIXEL_ID;
   if (!pixelId) {
     console.warn('Meta Pixel ID is not defined.');
     return;
   }
-  
-  
-  ReactPixel.init(pixelId, undefined as any, {
+
+  const ReactPixel = (await import('react-facebook-pixel')).default;
+
+  ReactPixel.init(pixelId, {} as any, {
     autoConfig: true,
     debug: false,
   } as any);
-  
+
   ReactPixel.pageView();
 };
 
-export const trackEvent = (eventName: string, data?: object) => {
-  if (typeof window !== 'undefined') {
-  
-    ReactPixel.track(eventName, data as any);
-  }
+export const trackEvent = async (eventName: string, data?: object) => {
+  if (typeof window === 'undefined') return;
+
+  const ReactPixel = (await import('react-facebook-pixel')).default;
+  ReactPixel.track(eventName, data as any);
 };
